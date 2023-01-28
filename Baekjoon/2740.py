59bca1c637cb42d0f5384a4fcc05839270be91cd
PictureHouse <fusma159@naver.com>
2023-01-28 23:48:47 +0900

    23.01.28

---
+++ Baekjoon/2740.py
@@ -0,0 +1,22 @@
+#행렬 곱셈 
+
+n, m = map(int, input().split())
+a = []
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+m, k = map(int, input().split())
+b = []
+for _ in range(m):
+    b.append(list(map(int, input().split())))
+
+result = [[0 for _ in range(k)] for _ in range(n)]
+for i in range(n):
+    for j in range(k):
+        for l in range(m):
+            result[i][j] += a[i][l] * b[l][j]
+
+for x in range(n):
+    for y in range(k):
+        print(result[x][y], end = " ")
+    print()
+
