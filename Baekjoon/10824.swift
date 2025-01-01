ce1e656fdd9c9f10f9e30b6a67e93becfb13544d
Yune Cho <fusma159@naver.com>
2025-01-01 21:41:58 +0900

    25.01.01

---
+++ Baekjoon/10824.swift
@@ -0,0 +1,8 @@
+// 네수
+
+let input = readLine()!.split(separator: " ")
+
+let first = Int("\(input[0])" + "\(input[1])")
+let second = Int("\(input[2])" + "\(input[3])")
+let result = first! + second!
+print(result)
